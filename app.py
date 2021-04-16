239656195e7603eb621f0632291cc3c48e3c76c9
fletchrk <77761223+fletchrk@users.noreply.github.com>
2021-04-15 18:07:56 -0800

    Mod 10 Challenge

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scraping
 
@@ -13,14 +13,14 @@ def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
 
+#scraping route
+
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
-   return redirect('/', code=302)
-
-.update(query_parameter, data, options)
+   return "Scraping Successful!"
 
 if __name__ == "__main__":
    app.run()
